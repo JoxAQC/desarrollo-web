aa9aa2c9130f14c1490907f020f8954acf688d16
Jose Quispe <jose.quispe35@unmsm.edu.pe>
2023-02-21 18:26:46 -0500

    Mobile menu fusion

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
+const burguerMenu = document.querySelector(".menu")
+const mobilMenu = document.querySelector(".mobile-menu")
 
 navEmail.addEventListener("click", toggleDesktopMenu)
+burguerMenu.addEventListener("click", toggleMobileMenu)
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive")
+}
+
+function toggleMobileMenu(){
+    mobilMenu.classList.toggle("inactive")
 }
