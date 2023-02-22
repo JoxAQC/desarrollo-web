da219306d66d83907be912d6fbfb1050a31372dd
Jose Quispe <jose.quispe35@unmsm.edu.pe>
2023-02-22 15:32:37 -0500

    Shopping cart fusion

--- main.js
+++ main.js
@@ -2,14 +2,23 @@ const navEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
 const burguerMenu = document.querySelector(".menu")
 const mobilMenu = document.querySelector(".mobile-menu")
+const cartIcon = document.querySelector(".navbar-shopping-cart")
+const shoppingCart = document.querySelector(".product-detail")
 
 navEmail.addEventListener("click", toggleDesktopMenu)
 burguerMenu.addEventListener("click", toggleMobileMenu)
+cartIcon.addEventListener("click", toggleShoppingCart)
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive")
 }
 
 function toggleMobileMenu(){
+    shoppingCart.classList.add("inactive")
     mobilMenu.classList.toggle("inactive")
+}
+
+function toggleShoppingCart(){
+    mobilMenu.classList.add("inactive")
+    shoppingCart.classList.toggle("inactive")
 }
